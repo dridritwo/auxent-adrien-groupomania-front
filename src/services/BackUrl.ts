2fb2aeaaf00b32ef156fc65c6b9320e25efda709
dridrione <a.auxent@gmail.com>
2021-11-03 16:21:37 +0100

    fixed slash

--- src/services/BackUrl.ts
+++ src/services/BackUrl.ts
@@ -1,9 +1,9 @@
 function getBackUrl() {
 
-    if (window.location.origin == "https://adrien-auxent-groupomania.herokuapp.com/" || window.location.origin == "https://quirky-booth-4f8e97.netlify.app") {
+    if (window.location.origin == "https://adrien-auxent-groupomania.herokuapp.com" || window.location.origin == "https://quirky-booth-4f8e97.netlify.app") {
         return "https://groupodb.herokuapp.com";
     } else {
-        return "http://localhost:3000/";
+        return "http://localhost:3000";
     }
 }
 
