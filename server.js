d0bf038e0370ac37c3d688beb3142c502f5a6179
Adrien Auxent <a.auxent@gmail.com>
2021-11-06 10:38:33 +0100

    fix name of app

--- server.js
+++ server.js
@@ -4,20 +4,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-<<<<<<< HEAD
 app.use(express.static(__dirname + '/dist'));
 
 app.get('/*', function(req,res) {
     
 res.sendFile(path.join(__dirname+'/dist/index.html'));
-=======
-app.use(express.static(__dirname + '/dist/<name-of-app>'));
-
-app.get('/*', function(req,res) {
-    
-res.sendFile(path.join(__dirname+'/dist/<name-of-app>/index.html'));
->>>>>>> 73f506d (added express server)
-});
 
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
